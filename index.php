775a2daf0e5d9201190a0097c322ff311f5ff7b7
Pedro Romano Barbosa <pdrobarbosa@gmail.com>
2015-12-02 08:09:17 +0000

    fixed a stupid bug

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset='UTF-8'>
 	<script src="jquery-1.11.3.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="Styles/Styles.css">
+	<link rel="stylesheet" type="text/css" href="Styles/styles.css">
 	<link rel="stylesheet" type="text/css" href="Styles/navigation.css">
 	<link rel="stylesheet" type="text/css" href="Styles/footer.css">
 	<link rel="stylesheet" type="text/css" href="Styles/main.css">
